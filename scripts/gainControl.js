e33b03676a1fa38b973dbe8334da520e68b571b5
Matthew Vaught <vaughtmt@gmail.com>
2014-11-01 11:42:04 -0400

    Correcting .js function conventions
    Test, web page operates correctly. Additionally disabled the controls
    for the audio source. Now the only way to interface is through the HTML
    buttons.

--- scripts/gainControl.js
+++ scripts/gainControl.js
@@ -9,13 +9,48 @@ var audioCtx,   //The context of the Web Audio API - neccesary for node linking
     btnMinus,   //HTML button for decreasing the gain in the gainNode
     slide;      //HTML input for controling gain, range 0 - 100
 
-function encapGain(value){
-    if(value > 1){
+
+function encapGain(value) {
+    "use strict";
+    if (value > 1) {
         value = 1;
-    }else if(value < 0){
+    } else if (value < 0) {
         value = 0;
     }
     return value;
+}
+
+function buttonPlay() {
+    "use strict";
+    source.mediaElement.play();
+}
+
+function buttonPause() {
+    "use strict";
+    source.mediaElement.pause();
+}
+
+function gainIncrease() {
+    "use strict";
+    gainValue += 0.1;
+    gainValue = encapGain(gainValue);
+    gainNode.gain.value = gainValue;
+    //console.log(gainNode.gain.value); 
+}
+
+function gainDecrease() {
+    "use strict";
+    gainValue -= 0.1;
+    gainValue = encapGain(gainValue);
+    gainNode.gain.value = gainValue;
+    //console.log(gainNode.gain.value); 
+}
+
+function gainSlider() {
+    "use strict";
+    gainValue = slide.valueAsNumber / 100.0;
+    gainNode.gain.value = gainValue;
+    //console.log(gainNode.gain.value); 
 }
 
 //Create an audio context from the HTML5 audio source
@@ -37,22 +72,16 @@ var btnPlay = document.getElementById("btnPlay");
 var btnStop = document.getElementById("btnStop");
 var btnPlus = document.getElementById("btnPlus");
 var btnMinus = document.getElementById("btnMinus");
-var slide = document.getElementById("gainSlide");    
+var slide = document.getElementById("gainSlide");
 
+//Assign functions to each button to execute on activation
+btnPlay.onclick = buttonPlay;
+btnStop.onclick = buttonPause;
+btnPlus.onclick = gainIncrease;
+btnMinus.onclick = gainDecrease;
 
-btnPlay.onclick = function () {source.mediaElement.play(); };
-
-
-btnStop.onclick = function () {source.mediaElement.pause(); };
-
-
-btnPlus.onclick = function () { gainValue += 0.1; encapGain(); gainNode.gain.value = gainValue; console.log(gainNode.gain.value); };
-
-
-btnMinus.onclick = function () { gainValue -= 0.1; encapGain();  gainNode.gain.value = gainValue; console.log(gainNode.gain.value); };
-
-
-slide.onchange = function () { gainValue = slide.valueAsNumber / 100.0; gainNode.gain.value = gainValue; console.log(gainNode.gain.value); };
+//Assign function to activate whenever the position of the slider changes
+slide.onchange = gainSlider;
 
 
 
