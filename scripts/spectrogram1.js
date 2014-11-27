45aacff0953db12865cb5a1a8fe1c4dd1d01a85e
Matthew Vaught <vaughtmt@gmail.com>
2014-11-26 20:30:02 -0500

    Hardcode Implementation of 3 Spectrograms
    Note that there is still the linear scale problem. Still looking for a
    way to be log.

--- scripts/spectrogram1.js
+++ scripts/spectrogram1.js
@@ -1,54 +1,55 @@
 
 
 //Draws the spectrogram onto the canvas
-function drawSpectrogram0(array) {
+function drawSpectrogram1(array) {
     "use strict";
     // copy the current canvas onto the temp canvas
-    tempCtx0.drawImage(canvas0, 0, 0, 400, 256);
+    tempCtx1.drawImage(canvas1, 0, 0, 400, 256);
 
     // iterate over the elements from the array
     for (var i = 0; i < array.length; i += 1) {
         // draw each pixel with the specific color
         var value = array[i];
-        ctx0.fillStyle = hot.getColor(value).hex();
+        ctx1.fillStyle = hot.getColor(value).hex();
 
         // draw the line at the right side of the canvas
-        ctx0.fillRect(400 - 1, 256 - i, 1, 1);
+        ctx1.fillRect(400 - 1, 256 - i, 1, 1);
     }
 
     // set translate on the canvas
-    ctx0.translate(-1, 0);
+    ctx1.translate(-1, 0);
     // draw the copied image
-    ctx0.drawImage(tempCanvas0, 0, 0, 400, 256, 0, 0, 400, 256);
+    ctx1.drawImage(tempCanvas1, 0, 0, 400, 256, 0, 0, 400, 256);
     // reset the transformation matrix
-    ctx0.setTransform(1, 0, 0, 1, 0, 0);
+    ctx1.setTransform(1, 0, 0, 1, 0, 0);
+    console.log("stuff");
 }
 
 //function called by the javascript node
-function audioProcess0() {
+function audioProcess1() {
     "use strict";
     // get the average for the first channel
-    var array = new Uint8Array(analyser0.frequencyBinCount);
-    analyser0.getByteFrequencyData(array);
+    var array = new Uint8Array(analyser1.frequencyBinCount);
+    analyser1.getByteFrequencyData(array);
 
     // draw the spectrogram
-    if (!source0.mediaElement.paused) {
+    if (!source1.mediaElement.paused) {
         //console.log(document.getElementById("src1").paused);
-        drawSpectrogram0(array);
+        drawSpectrogram1(array);
     }
 }
 
 //pull the canvas into a variable
-canvas0 = document.getElementById("canvas0");
+canvas1 = document.getElementById("canvas1");
 
 // get the audioCtx from the canvas to draw on
-ctx0 = $("#canvas0").get()[0].getContext("2d");
+ctx1 = $("#canvas1").get()[0].getContext("2d");
 
 // create a temp canvas we use for copying
-tempCanvas0 = document.createElement("canvas");
-tempCtx0 = tempCanvas0.getContext("2d");
-tempCanvas0.width = 400;
-tempCanvas0.height = 256;
+tempCanvas1 = document.createElement("canvas");
+tempCtx1 = tempCanvas1.getContext("2d");
+tempCanvas1.width = 400;
+tempCanvas1.height = 256;
 
 // used for color distribution
 hot = new chroma.ColorScale({
@@ -59,21 +60,21 @@ hot = new chroma.ColorScale({
 });
 
 // setup a javascript node [affects speed]
-javascriptNode0 = audioCtx.createScriptProcessor(2048, 1, 1);
+javascriptNode1 = audioCtx.createScriptProcessor(2048, 1, 1);
 // connect to destination, else it isn't called
-javascriptNode0.connect(audioCtx.destination);
+javascriptNode1.connect(audioCtx.destination);
 
 // setup a analyzer
-analyser0 = audioCtx.createAnalyser();
-analyser0.smoothingTimeConstant = 0;
-analyser0.fftSize = 512;
+analyser1 = audioCtx.createAnalyser();
+analyser1.smoothingTimeConstant = 0;
+analyser1.fftSize = 512;
 
 // create a buffer source node
-gainNode0.connect(analyser0);
-analyser0.connect(javascriptNode0);
+gainNode1.connect(analyser1);
+analyser1.connect(javascriptNode1);
 
 
 // when the javascript node is called
 // we use information from the analyzer node
 // to draw the volume
-javascriptNode0.onaudioprocess = audioProcess0;
+javascriptNode1.onaudioprocess = audioProcess1;
--- scripts/spectrogram2.js
+++ scripts/spectrogram2.js
@@ -1,54 +1,54 @@
 
 
 //Draws the spectrogram onto the canvas
-function drawSpectrogram0(array) {
+function drawSpectrogram2(array) {
     "use strict";
     // copy the current canvas onto the temp canvas
-    tempCtx0.drawImage(canvas0, 0, 0, 400, 256);
+    tempCtx2.drawImage(canvas2, 0, 0, 400, 256);
 
     // iterate over the elements from the array
     for (var i = 0; i < array.length; i += 1) {
         // draw each pixel with the specific color
         var value = array[i];
-        ctx0.fillStyle = hot.getColor(value).hex();
+        ctx2.fillStyle = hot.getColor(value).hex();
 
         // draw the line at the right side of the canvas
-        ctx0.fillRect(400 - 1, 256 - i, 1, 1);
+        ctx2.fillRect(400 - 1, 256 - i, 1, 1);
     }
 
     // set translate on the canvas
-    ctx0.translate(-1, 0);
+    ctx2.translate(-1, 0);
     // draw the copied image
-    ctx0.drawImage(tempCanvas0, 0, 0, 400, 256, 0, 0, 400, 256);
+    ctx2.drawImage(tempCanvas2, 0, 0, 400, 256, 0, 0, 400, 256);
     // reset the transformation matrix
-    ctx0.setTransform(1, 0, 0, 1, 0, 0);
+    ctx2.setTransform(1, 0, 0, 1, 0, 0);
 }
 
 //function called by the javascript node
-function audioProcess0() {
+function audioProcess2() {
     "use strict";
     // get the average for the first channel
-    var array = new Uint8Array(analyser0.frequencyBinCount);
-    analyser0.getByteFrequencyData(array);
+    var array = new Uint8Array(analyser2.frequencyBinCount);
+    analyser2.getByteFrequencyData(array);
 
     // draw the spectrogram
-    if (!source0.mediaElement.paused) {
+    if (!source2.mediaElement.paused) {
         //console.log(document.getElementById("src1").paused);
-        drawSpectrogram0(array);
+        drawSpectrogram2(array);
     }
 }
 
 //pull the canvas into a variable
-canvas0 = document.getElementById("canvas0");
+canvas2 = document.getElementById("canvas2");
 
 // get the audioCtx from the canvas to draw on
-ctx0 = $("#canvas0").get()[0].getContext("2d");
+ctx2 = $("#canvas2").get()[0].getContext("2d");
 
 // create a temp canvas we use for copying
-tempCanvas0 = document.createElement("canvas");
-tempCtx0 = tempCanvas0.getContext("2d");
-tempCanvas0.width = 400;
-tempCanvas0.height = 256;
+tempCanvas2 = document.createElement("canvas");
+tempCtx2 = tempCanvas2.getContext("2d");
+tempCanvas2.width = 400;
+tempCanvas2.height = 256;
 
 // used for color distribution
 hot = new chroma.ColorScale({
@@ -59,21 +59,21 @@ hot = new chroma.ColorScale({
 });
 
 // setup a javascript node [affects speed]
-javascriptNode0 = audioCtx.createScriptProcessor(2048, 1, 1);
+javascriptNode2 = audioCtx.createScriptProcessor(2048, 1, 1);
 // connect to destination, else it isn't called
-javascriptNode0.connect(audioCtx.destination);
+javascriptNode2.connect(audioCtx.destination);
 
 // setup a analyzer
-analyser0 = audioCtx.createAnalyser();
-analyser0.smoothingTimeConstant = 0;
-analyser0.fftSize = 512;
+analyser2 = audioCtx.createAnalyser();
+analyser2.smoothingTimeConstant = 0;
+analyser2.fftSize = 512;
 
 // create a buffer source node
-gainNode0.connect(analyser0);
-analyser0.connect(javascriptNode0);
+gainNode2.connect(analyser2);
+analyser2.connect(javascriptNode2);
 
 
 // when the javascript node is called
 // we use information from the analyzer node
 // to draw the volume
-javascriptNode0.onaudioprocess = audioProcess0;
+javascriptNode2.onaudioprocess = audioProcess2;
