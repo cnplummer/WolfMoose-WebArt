af99cede7b48c7bf79ec942bea22838e92001cbe
Paul Kirby <paulrdkirby@gmail.com>
2015-10-07 20:14:44 -0600

    Optimized audio.js further, added the slider.js into it. Still call as async

--- scripts/audio.js
+++ scripts/audio.js
@@ -341,3 +341,48 @@ initializeVariables();
 gainNode0.connect(javascriptNode0);
 gainNode1.connect(javascriptNode1);
 gainNode2.connect(javascriptNode2);
+
+
+// Connect sliders to gain nodes, then set their initial position
+$(".slider1").slider({
+    min: 0,
+    max: 100,
+    value: 0,
+    animate: "true",
+    orientation: "horizontal",
+    slide: function(event, ui) {
+    setVolume(gainNode0, ui.value / 20);
+    }
+});
+$(".slider2").slider({
+    min: 0,
+    max: 100,
+    value: 0,
+    orientation: "horizontal",
+    animate: "true",
+    slide: function(event, ui) {
+    setVolume(gainNode1, ui.value / 10);
+    }
+});
+$(".slider3").slider({
+    min: 0,
+    max: 100,
+    value: 0,
+    orientation: "horizontal",
+    animate: "true",
+    slide: function(event, ui) {
+    setVolume(gainNode2, ui.value / 12);
+    }
+});
+function setVolume(node, myGain) {
+node.gain.value = myGain;
+};
+
+setTimeout(function() {
+    $(".slider1").slider("value", 20);
+    gainNode0.gain.value = 1;
+    $(".slider2").slider("value", 20);
+    gainNode1.gain.value = 2;
+    $(".slider3").slider("value", 24);
+    gainNode2.gain.value = 2;
+}, 2000);
